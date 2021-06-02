1140f8a23da19d6ad1cb560e9b9b513b9b7dae31
kishu1998 <kishorsanjaypatil1998@gmail.com>
2021-06-03 00:02:25 +0530

    my project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # doodleblue-kishor-service
+
+#project start(no password hashing done for time being)
+setup .env config 
+npm install
+npm run start
--- index.js
+++ index.js
@@ -8,7 +8,30 @@ const app =express();
 
 app.use(cors());
 app.use(express.json());
-
+app.get('/users',aunthenticateToken,async(req,res)=>{
+    try{
+    const connection = await mysql.connection();
+    let users = await connection.query("SELECT * FROM users");
+    res.json(users);
+    }
+    catch(err){
+        return res.sendStatus(403);  
+    }
+});
+app.post('/signup',async(req,res)=>{
+    try{
+        let {password,email,firstName,lastName,Address}=req.body;
+    const connection = await mysql.connection();//INSERT INTO users (firstName,lastName,email,pwd,address) VALUES ?
+    let result = await connection.query("INSERT INTO users SET ?",{firstName,lastName,email,pwd:password,Address});
+    await connection.query("commit");
+    const accessToken= jwt.sign(email,process.env.ACCESS_TOKEN);
+    res.json({accessToken,user:{userId:result.insertId,email,firstName,lastName,Address}});
+    }
+    catch(err){
+        console.log(error);
+        return res.sendStatus(403);  
+    }
+});
 app.post('/login',async (req,res)=>{
     const connection = await mysql.connection();
     let users = await connection.query("SELECT * FROM users WHERE email = ?", [req.body.email]);
@@ -17,17 +40,26 @@ app.post('/login',async (req,res)=>{
     const password=req.body.password;
     const user={userName};
     //mysql.query();
-   const accessToken= jwt.sign(user,process.env.ACCESS_TOKEN);
-   res.json({accessToken});
+    if(users && users[0] && users[0].pwd==password)
+   {    const accessToken= jwt.sign(user,process.env.ACCESS_TOKEN);
+    res.json({accessToken,user:users[0]});
+   }
+   else res.json({error:"Authentication"});
 });
 
 function aunthenticateToken(req,res,next){
-        const token=req.headers['authorization'];
+    try{  
+    const token=req.headers['authorization'].split(' ')[1];
         if(token==null) return res.sendStatus(401);
         jwt.verify(token,process.env.ACCESS_TOKEN,(err,user)=>{
         if(err) return res.sendStatus(403);
-            req.user=user;
+
         next();
-})
+})}
+catch(err)
+{
+    return res.sendStatus(403);
+    
+}
 }
 app.listen(3012)
